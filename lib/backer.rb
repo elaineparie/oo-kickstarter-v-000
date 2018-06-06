3bbdbe4b59dbf9b2e69406909b2920ce42f776fa
Elaine Anderson <eparie@bu.edu>
2018-06-06 03:22:31 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,7 +1,7 @@
 require 'pry'
 class Backer
 
-  attr_accessor :name, :project, :title, :backed_projects, :backers
+  attr_accessor :name, :title, :backed_projects, :backers
 
   def initialize(name)
     @name = name
