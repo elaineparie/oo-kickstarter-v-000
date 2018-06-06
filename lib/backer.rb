321eddfe5daa5b398f4207668c5f332bcdac0c12
Elaine Anderson <eparie@bu.edu>
2018-06-06 02:13:23 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,17 @@
+require 'pry'
+class Backer
+
+  attr_accessor :name, :backed_projects, :project, :title
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+def back_project(project)
+#  project = Project.new(title)
+  @backed_projects << project
+ backer.backers.backer = self
+end
+
+end
---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+
+
+class Project
+  attr_accessor :title, :backers, :backer, :project
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+def add_backer(backer)
+  @backers << backer
+  backer.project = self
+end
+
+end
