a0089a3bd00d78eb197bdbb774ba95cb127da3a7
Elaine Anderson <eparie@bu.edu>
2018-06-06 03:19:31 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,7 @@
 
 
 class Project
-  attr_accessor :title, :backer, :project, :backers
+  attr_accessor :title, :backer, :project, :backers, :backed_projects
 
   def initialize(title)
     @title = title
